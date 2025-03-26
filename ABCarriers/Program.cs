419b00abdbb936d9386413dbaaeee01587c57de7
enigmasheet <abhaymandal321@gmail.com>
2025-03-26 15:51:10 +0545

    usedeveloperexceptionpage added

--- ABCarriers/Program.cs
+++ ABCarriers/Program.cs
@@ -25,6 +25,10 @@ namespace ABCarriers
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            else
+            {
+                app.UseDeveloperExceptionPage(); // Shows detailed errors in development
+            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
