071b25f27fb8dab06d0267feb6f6570b22ffb796
enigmasheet <abhaymandal321@gmail.com>
2025-03-27 13:32:28 +0545

    nepali date picker in invoices create and laywoud andd datepicker.js

---
+++ ABCarriers/wwwroot/js/datePicker.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    // Initialize Nepali Date Picker
+    $('#miti').nepaliDatePicker({
+        dateFormat: "%y-%m-%d",
+        closeOnDateSelect: true,
+        defaultDate: "",               // Set a default date (optional)
+        minDate: "२०००-०१-०१",
+        maxDate: "२०८०-१२-३०"
+    });
+});
